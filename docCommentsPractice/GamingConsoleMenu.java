e943d5fe054f60edfcf2192cb99423d47b3755a4
emefjay <43625176+emefjay@users.noreply.github.com>
2021-09-11 19:38:08 -0600

    Update GamingConsoleMenu.java

--- docCommentsPractice/GamingConsoleMenu.java
+++ docCommentsPractice/GamingConsoleMenu.java
@@ -1,14 +1,25 @@
 package docCommentsPractice;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author emmajuvenal
+ * @author Jaland Aland
+ * @author Dorian Glines
+ * 
+ *         Class for accessing and displaying Gaming Console Objects. Includes a
+ *         private menuPrompt method and the main method.
+ */
+
 public class GamingConsoleMenu {
-	
-	
-	private static void menuPrompt()
-	{
+
+	/**
+	 * Method for the menu display.
+	 */
+
+	private static void menuPrompt() {
 		System.out.println("1. Show all gaming consoles");
 		System.out.println("2. Add a device");
 		System.out.println("3. Find a device");
@@ -17,110 +28,91 @@ public class GamingConsoleMenu {
 		System.out.println("6. Exit");
 		System.out.print("Enter your selection: ");
 	}
-	
-	public static void main(String[] args)
-	{
 
-	ArrayList<GamingConsole> consoles = new ArrayList<GamingConsole>();
-		consoles.add(new GamingConsole ("Switch","Nintendo",2017) );
-		consoles.add(new GamingConsole ("Xbox Series x", "Microsoft", 2020) );
-		consoles.add(new GamingConsole ("Playstation 5", "Sony", 2020) );
-		
-		
-		
+	/**
+	 * @param args
+	 * 
+	 *             The main method. Declares and initializes GamingConsole objects.
+	 *             Includes a switch statement with the code for the menuPrompt.
+	 */
+	public static void main(String[] args) {
+
+		ArrayList<GamingConsole> consoles = new ArrayList<GamingConsole>();
+		consoles.add(new GamingConsole("Switch", "Nintendo", 2017));
+		consoles.add(new GamingConsole("Xbox Series x", "Microsoft", 2020));
+		consoles.add(new GamingConsole("Playstation 5", "Sony", 2020));
+
 		Scanner input = new Scanner(System.in);
 		int choice;
-		do 
-		{
+		do {
 			menuPrompt();
 			choice = input.nextInt();
+			input.nextLine();
 			int idNumber;
-			
-			switch(choice)
-			{
-				case 1:
-					for(int i = 0; i < consoles.size(); i++)
-					{
-						System.out.println(consoles.get(i));
+
+			switch (choice) {
+
+			case 1:
+				for (int i = 0; i < consoles.size(); i++) {
+					System.out.println(consoles.get(i));
+					System.out.println();
+				}
+				break;
+			case 2:
+
+				String consoleName;
+				String companyName;
+				int consoleYear;
+				int selection;
+
+				System.out.println("What is the name of the console you would like to add?");
+				consoleName = input.nextLine();
+
+				System.out.println("What is the name of console's comapny?");
+				companyName = input.nextLine();
+
+				System.out.println("What year was the console made?");
+				consoleYear = input.nextInt();
+
+				GamingConsole newConsole = new GamingConsole(consoleName, companyName, consoleYear);
+				consoles.add(newConsole);
+				break;
+
+			case 3: {
+				System.out.println();
+				System.out.println("Id: ");
+				idNumber = input.nextInt();
+
+				for (int i = 0; i < consoles.size(); i++) {
+					if (consoles.get(i).getUniqueId() == idNumber) {
+						System.out.println(consoles.get(i).toString());
 						System.out.println();
+						break;
 					}
-					break;
-				case 2: 
-					addConsole();
-					consoles.add();
-					break;
-				case 3: 
-					{
-						System.out.println();
-						System.out.println("Id: ");
-						idNumber = input.nextInt();
-						
-						for(int i =0; i < 3; i++)
-						{
-							@SuppressWarnings("unlikely-arg-type")
-							boolean retval = consoles.contains(idNumber); 
-					        
-						      if (retval == true) 
-						      {
-						         System.out.println(consoles.get(i));
-						      } else 
-						      	{
-						    	  	System.out.printf("Id number %d was not found", idNumber);
-						      	}
-						}
-						
-						System.out.println();
-						
-					}
-					break;
-				case 4: 
-					break;
-				case 5:
-					{
-						System.out.println();
-						System.out.println("Number of consoles: " + consoles.size());
-						System.out.println();
-					}
-					break;
-				case 6: 
-					{
-						System.out.println();
-						System.out.println("Goodbye");
-					}
-					break;
-				default:
-					
-				
+
+				}
 			}
-			
-		}while(choice != 6);
-		
-		
-	}	
-		/**
-		 * 
-		 * @return
-		 */
-		private static GamingConsole addConsole() {
-		
-		Scanner input = new Scanner(System.in);
-		String consoleName;
-		String companyName;
-		int consoleYear;
-		int selection;
-		
-			
-		System.out.println("What is the name of the console you would like to add?");
-		consoleName = input.nextLine();
-		
-		System.out.println("What is the name of console's comapny?");
-		companyName = input.nextLine();
-		
-		System.out.println("What year was the console made?");
-		consoleYear = input.nextInt();
-		
-		return new GamingConsole(consoleName, companyName, consoleYear);
-			
-		}
+
+			case 4: {
 	
+
+			}
+				break;
+			case 5: {
+				System.out.println();
+				System.out.println("Number of consoles: " + consoles.size());
+				System.out.println();
+			}
+				break;
+			case 6: {
+				System.out.println();
+				System.out.println("Goodbye");
+			}
+				break;
+
+			}
+
+		} while (choice != 6);
+
+	}
 }
